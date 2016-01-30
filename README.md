1adb2267fdf64292c1627b53638fb270bc4d0aa5
mateogianolio <gianoliomateo@gmail.com>
2016-01-30 16:13:05 +0100

    updates

---
+++ README.md
@@ -0,0 +1,21 @@
+# openblog
+
+Opening the doors to true open-source blogging by utilizing Github Pages hosting and Github Issues as storage media for posts and comments.
+
+This is just an early prototype and would probably benefit from request caching and pagination.
+
+### Setup
+
+1. To increase rate limit, create a Github Application and then [follow this simple guide](https://developer.github.com/changes/2012-12-06-create-authorization-for-app/) to generate an OAuth token without needing to use the webflow.
+
+2. Change the globals in `lib/globals.js` to suit your needs:
+
+```javascript
+(function () {
+  'use strict';
+
+  window.USERNAME = 'your username here';
+  window.REPO = 'your repo name here';
+  window.TOKEN = 'your app access token here';
+}());
+```
--- js/auth.js
+++ js/auth.js
@@ -9,50 +9,13 @@
 
     // export API as global
     window.github = new Github({
-      username: cookies.username,
-      password: cookies.password,
-      auth: 'basic'
+      token: window.TOKEN,
+      auth: 'oauth'
     });
 
-    var user = window.github.getUser();
-    user.show(cookies.username, function (error, user) {
-      if (error) {
-        // use personal access token if authentication throws error
-        window.github = new Github({
-          token: window.TOKEN,
-          auth: 'oauth'
-        });
-
-        // clear cookies
-        setCookie('username', '', 0);
-        setCookie('password', '', 0);
-      }
-
-      // render
-      window.github
-        .getIssues(window.USERNAME, window.REPO)
-        .list({}, window.ghRender);
-    });
-  };
-
-  // called on login form submit
-  window.ghLogin = function (event) {
-    event.preventDefault();
-
-    var form = document.querySelector('.login'),
-        username = form.username.value,
-        password = form.password.value;
-
-    if (!username || !password)
-      return false;
-
-    setCookie('username', username);
-    setCookie('password', password);
-
-    // authenticate
-    ghAuth();
-
-    // prevent default event
-    return false;
+    // render
+    window.github
+      .getIssues(window.USERNAME, window.REPO)
+      .list({}, window.ghRender);
   };
 }());
