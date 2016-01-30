e1015d40ddac2638ef736195723beb2213b40912
mateogianolio <gianoliomateo@gmail.com>
2016-01-30 15:58:28 +0100

    upd repo name

--- js/globals.js
+++ js/globals.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.USERNAME = 'mateogianolio'; // github username
-  window.REPO = 'issue-forum'; // github repository
+  window.REPO = 'openblog'; // github repository
   window.TOKEN = 'e70eebb2b683a22e6b4403f8fa3423927404068b'; // github app access token
 }());
