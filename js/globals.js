f3b3f1ed40fa99852525a8289770c898e12df5f1
mateogianolio <gianoliomateo@gmail.com>
2016-02-13 15:54:02 +0100

    cleanup

--- js/globals.js
+++ js/globals.js
@@ -1,7 +1,5 @@
-(function () {
-  'use strict';
+'use strict';
 
-  window.USERNAME = 'mateogianolio'; // github username
-  window.REPO = 'openblog'; // github repository
-  window.TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
-}());
+var USERNAME = 'mateogianolio', // github username
+    REPO = 'openblog', // github repository
+    TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
--- js/render.js
+++ js/render.js
@@ -1,124 +1,86 @@
-(function () {
-  'use strict';
+'use strict';
 
-  // default sorting order
-  function order(a, b) {
-    return a.updatedAt < b.updatedAt ?
-      1 : a.updatedAt === b.updatedAt ?
-      0 : -1;
-  }
+var github = new Github({
+  token: TOKEN,
+  auth: 'oauth'
+});
 
-  function html(type, content) {
-    var element = document.createElement(type);
-    if (content instanceof HTMLElement)
-      element.appendChild(content);
-    else if (content)
-      element.innerHTML = content;
-    return element;
-  }
+// default sorting order
+function order(a, b) {
+  return a.updatedAt < b.updatedAt ?
+    1 : a.updatedAt === b.updatedAt ?
+    0 : -1;
+}
 
-  function link(href, content) {
-    var element = html('a', content);
-    element.href = href;
-    return element;
-  }
+// generates html from a github issue
+function generate(issue) {
+  // skip issues from other users than yourself
+  if (issue.user.login !== window.USERNAME)
+    return;
 
-  // generates html from a github issue
-  function generate(issue) {
-    // skip issues from other users than yourself
-    if (issue.user.login !== window.USERNAME)
-      return;
+  var parent = document.querySelector('.threads'),
+      content = [];
 
-    var parent = document.querySelector('.threads'),
-        container = html('div');
+  content.push('<div id="' + issue.id + '" class="post">');
+  content.push('<h1 class="post-title">');
+  content.push('<a href="' + issue.html_url + '">' + issue.title + '</a>');
+  content.push('</h1>');
+  content.push('<div class="post-meta-categories">');
+  issue.labels.forEach(function (label) {
+    content.push('<span class="post-meta-category" style="background: #' + label.color + '">');
+    content.push(label.name);
+    content.push('</span>');
+  });
+  content.push('</div>');
+  content.push('<div class="post-meta-date">');
+  content.push(new Date(issue.created_at).toLocaleDateString());
+  content.push('</div>');
+  content.push('<div class="post-body">');
+  content.push(marked(issue.body));
+  content.push('</div>');
+  content.push('<div class="post-comments">');
+  content.push('Loading ' + issue.comments + ' comments...');
+  content.push('</div>');
+  content.push('</div>');
+  parent.innerHTML = content.join('');
 
-    container.className = 'post';
+  if (issue.comments) {
+    github._request('GET', issue.comments_url, {}, function (error, data) {
+      if (error)
+        return;
 
-    parent.appendChild(container);
+      var commentContainer = document.querySelector('.post-comments');
+      commentContainer.innerHTML = '';
 
-    var title = html('h1', link(issue.html_url, issue.title));
-    title.className = 'post-title';
-    container.appendChild(title);
-
-    var categories = html('div');
-    categories.className = 'post-meta-categories';
-    title.appendChild(categories);
-
-    issue.labels.forEach(function (label) {
-      var element = html('span', label.name);
-      element.className = 'post-meta-category';
-      element.style.background = '#' + label.color;
-      categories.appendChild(element);
-    });
-
-    var date = html('div', new Date(issue.created_at).toLocaleDateString());
-    date.className = 'post-meta-date';
-    container.appendChild(date);
-
-    var body = html('div', marked(issue.body));
-    body.className = 'post-body';
-    container.appendChild(body);
-
-    var comments = html('div');
-    comments.className = 'post-comments';
-    container.appendChild(comments);
-
-    if (issue.comments) {
-      var count = html('div', 'Loading ' + issue.comments + ' comment(s)...');
-      comments.appendChild(count);
-
-      window.github._request(
-        'GET',
-        issue.comments_url,
-        {},
-        function (error, data) {
-          if (error)
-            return;
-
-          comments.innerHTML = '';
-
-          data.forEach(function(comment) {
-            var element = html('div');
-            element.id = comment.id;
-            element.className = 'post-comment';
-            comments.appendChild(element);
-
-            var author = html('div', link(comment.user.html_url, comment.user.login));
-            author.className = 'post-comment-author';
-            element.appendChild(author);
-
-            var date = html('span', new Date(comment.created_at).toLocaleDateString());
-            date.className = 'post-comment-date';
-            author.appendChild(date);
-
-            var body = html('div', marked(comment.body));
-            body.className = 'post-comment-body';
-            element.appendChild(body);
-          });
-
-          comments.appendChild(link(issue.html_url, 'add comment'));
-        }
-      );
-    }
-
-    // attribs
-    container.id = issue.id;
-    link.href = issue.html_url;
-    comments.href = issue.html_url;
-  }
-
-  // renders github issues
-  window.render = function () {
-    window.github
-      .getIssues(window.USERNAME, window.REPO)
-      .list({}, function (error, issues) {
-        if (error || !issues)
-          return;
-
-        document.querySelector('.threads').innerHTML = '';
-        issues
-          .sort(order)
-          .forEach(generate);
+      var comments = [];
+      data.forEach(function (comment) {
+        comments.push('<div id="' + comment.id + '" class="post-comment">');
+        comments.push('<a class="post-comment-author" href="' + comment.user.html_url + '">');
+        comments.push(comment.user.login);
+        comments.push('</a>');
+        comments.push('<span class="post-comment-date">');
+        comments.push(new Date(comment.created_at).toLocaleDateString());
+        comments.push('</span>');
+        comments.push('<div class="post-comment-body">');
+        comments.push(marked(comment.body));
+        comments.push('</div>');
+        comments.push('</div>');
       });
-  };
-}());
+
+      comments.push('<a href="' + issue.html_url + '">add comment</a>');
+      commentContainer.innerHTML += comments.join('');
+    });
+  }
+}
+
+// renders github issues
+function render() {
+  github
+    .getIssues(USERNAME, REPO)
+    .list({}, function (error, issues) {
+      if (error || !issues)
+        return;
+
+      issues.sort(order).forEach(generate);
+    });
+}
