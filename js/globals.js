e2f53659f0545860db4940128b063f7754b35ef9
Mateo Gianolio <gianoliomateo@gmail.com>
2020-02-11 01:20:54 +0100

    cleanup

--- js/globals.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var USERNAME = 'mateogianolio', // github username
-    REPO = 'openblog', // github repository
-    TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
